338817016b06f79d790c9b3f53ac6fb4995985fa
xinyuncui <xinyun.cui123@gmail.com>
2025-01-15 11:25:56 -0800

    backend employee 1 and 2

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { login, signup } from "../controller/authController.js";
+import { login, refreshPage, signup } from "../controller/authController.js";
 
 const router = Router();
 router.post("/signup/:token", signup);
 router.post("/login", login);
+router.post("/refresh", refreshPage);
 
 export default router;
