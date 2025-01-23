c469bb61f5b8e01bd6fbf2cae64d7109856e871a
xinyuncui <xinyun.cui123@gmail.com>
2025-01-22 23:48:01 -0800

    small fix + 2nd section in visa status

--- src/pages/HR/VisaStatusPage.jsx
+++ src/pages/HR/VisaStatusPage.jsx
@@ -17,6 +17,7 @@ import {
   DialogContentText,
   DialogTitle,
   InputLabel,
+  Link,
 } from "@mui/material";
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -120,7 +121,7 @@ const HRVisaStatusManagementPage = () => {
       </Typography>
       <TableContainer
         component={Paper}
-        sx={{ maxHeight: 800, overflowY: "auto", mb: 3 }}
+        sx={{ maxHeight: 300, overflowY: "auto", mb: 3 }}
       >
         <Table stickyHeader>
           <TableHead>
@@ -131,6 +132,212 @@ const HRVisaStatusManagementPage = () => {
               <TableCell sx={{ width: "15%" }}>Remaining Days</TableCell>
               <TableCell sx={{ width: "25%" }}>Next Step</TableCell>
               <TableCell sx={{ width: "20%" }}>Action</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {loading ? (
+              <TableRow>
+                <TableCell colSpan={3} align="center">
+                  <CircularProgress size={24} />
+                </TableCell>
+              </TableRow>
+            ) : all.length > 0 ? (
+              all.map((app, i) =>
+                app.visaType === "OPT" ? (
+                  <TableRow key={i}>
+                    <TableCell sx={{ width: "20%" }}>
+                      {app.user.profile.name.firstName +
+                        app.user.profile.name.lastName}
+                    </TableCell>
+                    <TableCell sx={{ width: "15%" }}>{app.visaType}</TableCell>
+                    <TableCell sx={{ width: "25%" }}>
+                      {app.user.profile.residency.workAuthorization.startDate} -{" "}
+                      {app.user.profile.residency.workAuthorization.endDate}
+                    </TableCell>
+                    <TableCell sx={{ width: "10%" }}>
+                      {Math.ceil(
+                        (new Date(
+                          app.user.profile.residency.workAuthorization.endDate
+                        ) -
+                          currentDate) /
+                          (1000 * 60 * 60 * 24)
+                      )}
+                    </TableCell>
+                    <TableCell sx={{ width: "30%" }}>
+                      {app.documents && app.documents.length > 0
+                        ? (() => {
+                            const lastDoc =
+                              app.documents[app.documents.length - 1];
+                            console.log(lastDoc.status);
+                            switch (lastDoc.status) {
+                              case "Pending":
+                                return "waiting for HR approval";
+                              case "Approved":
+                                return nextStep[lastDoc.type] || "All Set";
+                              case "Rejected":
+                                return "re-upload " + lastDoc.type;
+                              default:
+                                return "Unknown status";
+                            }
+                          })()
+                        : "upload OPT Receipt"}
+                    </TableCell>
+                    {/* action column */}
+                    <TableCell sx={{ width: "20%" }}>
+                      {app.documents && app.documents.length > 0 ? (
+                        (() => {
+                          const lastDoc =
+                            app.documents[app.documents.length - 1];
+                          switch (lastDoc.status) {
+                            case "Pending":
+                              return (
+                                <>
+                                  <Button
+                                    variant="outlined"
+                                    size="small"
+                                    sx={{ marginBottom: 1 }}
+                                    component="a"
+                                    href={lastDoc.previewLink} // Ensure `previewLink` exists in your document object
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                  >
+                                    Preview
+                                  </Button>
+
+                                  {/* Bottom section: Approve/Reject */}
+                                  <div
+                                    style={{
+                                      display: "flex",
+                                      justifyContent: "space-between",
+                                    }}
+                                  >
+                                    <Button
+                                      variant="contained"
+                                      color="success"
+                                      size="small"
+                                      onClick={() => handleApprove(lastDoc._id)}
+                                    >
+                                      Approve
+                                    </Button>
+                                    <Button
+                                      variant="contained"
+                                      color="error"
+                                      size="small"
+                                      onClick={handleOpen}
+                                    >
+                                      Reject
+                                    </Button>
+                                  </div>
+                                  {/* Feedback Dialog */}
+                                  <Dialog open={open} onClose={handleClose}>
+                                    <DialogTitle>Reject Document</DialogTitle>
+                                    <DialogContent>
+                                      <DialogContentText>
+                                        Please provide feedback for rejecting
+                                        this document.
+                                      </DialogContentText>
+                                      <TextField
+                                        autoFocus
+                                        margin="dense"
+                                        label="Feedback"
+                                        fullWidth
+                                        multiline
+                                        rows={4}
+                                        variant="outlined"
+                                        value={feedback}
+                                        onChange={(e) =>
+                                          setFeedback(e.target.value)
+                                        }
+                                      />
+                                    </DialogContent>
+                                    <DialogActions>
+                                      <Button
+                                        onClick={handleClose}
+                                        color="secondary"
+                                      >
+                                        Cancel
+                                      </Button>
+                                      <Button
+                                        onClick={() =>
+                                          handleReject(lastDoc._id)
+                                        }
+                                        variant="contained"
+                                        color="error"
+                                        disabled={!feedback.trim()} // Disable if feedback is empty
+                                      >
+                                        Submit
+                                      </Button>
+                                    </DialogActions>
+                                  </Dialog>
+                                </>
+                              );
+                            case "Approved":
+                              return (
+                                <Button
+                                  variant="contained"
+                                  color="primary"
+                                  size="small"
+                                  onClick={() =>
+                                    handleSendNotification(
+                                      app.user.email,
+                                      nextStep[lastDoc.type]
+                                    )
+                                  }
+                                >
+                                  Send Notification
+                                </Button>
+                              );
+                            default:
+                              return null;
+                          }
+                        })()
+                      ) : (
+                        <Button
+                          variant="contained"
+                          color="primary"
+                          size="small"
+                          onClick={() =>
+                            handleSendNotification(
+                              app.user.email,
+                              "OPT Receipt"
+                            )
+                          }
+                        >
+                          Send Notification
+                        </Button>
+                      )}
+                    </TableCell>
+                  </TableRow>
+                ) : null
+              )
+            ) : (
+              <TableRow>
+                <TableCell colSpan={3} align="center">
+                  No pending applications
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      {/* all emplyee status */}
+      <Typography variant="h6" sx={{ mb: 2 }}>
+        Visa Status
+      </Typography>
+      <TableContainer
+        component={Paper}
+        sx={{ maxHeight: 300, overflowY: "auto", mb: 3 }}
+      >
+        <Table stickyHeader>
+          <TableHead>
+            <TableRow>
+              <TableCell sx={{ width: "20%" }}>Full Name</TableCell>
+              <TableCell sx={{ width: "15%" }}>VisaTitle</TableCell>
+              <TableCell sx={{ width: "25%" }}>Start/End Date</TableCell>
+              <TableCell sx={{ width: "15%" }}>Remaining Days</TableCell>
+              <TableCell sx={{ width: "20%" }}>Next Step</TableCell>
+              <TableCell sx={{ width: "30%" }}>Documents</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -161,8 +368,10 @@ const HRVisaStatusManagementPage = () => {
                         (1000 * 60 * 60 * 24)
                     )}
                   </TableCell>
-                  <TableCell sx={{ width: "30%" }}>
-                    {app.documents && app.documents.length > 0
+                  <TableCell sx={{ width: "20%" }}>
+                    {app.visaType === "OPT" &&
+                    app.documents &&
+                    app.documents.length > 0
                       ? (() => {
                           const lastDoc =
                             app.documents[app.documents.length - 1];
@@ -178,126 +387,28 @@ const HRVisaStatusManagementPage = () => {
                               return "Unknown status";
                           }
                         })()
-                      : "upload OPT Receipt"}
+                      : app.visaType === "OPT"
+                      ? "upload OPT Receipt"
+                      : null}
                   </TableCell>
-                  {/* action column */}
-                  <TableCell sx={{ width: "20%" }}>
-                    {app.documents && app.documents.length > 0 ? (
-                      (() => {
-                        const lastDoc = app.documents[app.documents.length - 1];
-                        switch (lastDoc.status) {
-                          case "Pending":
-                            return (
-                              <>
-                                <Button
-                                  variant="outlined"
-                                  size="small"
-                                  sx={{ marginBottom: 1 }}
-                                  component="a"
-                                  href={lastDoc.previewLink} // Ensure `previewLink` exists in your document object
-                                  target="_blank"
-                                  rel="noopener noreferrer"
-                                >
-                                  Preview
-                                </Button>
+                  {/* document column */}
 
-                                {/* Bottom section: Approve/Reject */}
-                                <div
-                                  style={{
-                                    display: "flex",
-                                    justifyContent: "space-between",
-                                  }}
-                                >
-                                  <Button
-                                    variant="contained"
-                                    color="success"
-                                    size="small"
-                                    onClick={() => handleApprove(lastDoc._id)}
-                                  >
-                                    Approve
-                                  </Button>
-                                  <Button
-                                    variant="contained"
-                                    color="error"
-                                    size="small"
-                                    onClick={handleOpen}
-                                  >
-                                    Reject
-                                  </Button>
-                                </div>
-                                {/* Feedback Dialog */}
-                                <Dialog open={open} onClose={handleClose}>
-                                  <DialogTitle>Reject Document</DialogTitle>
-                                  <DialogContent>
-                                    <DialogContentText>
-                                      Please provide feedback for rejecting this
-                                      document.
-                                    </DialogContentText>
-                                    <TextField
-                                      autoFocus
-                                      margin="dense"
-                                      label="Feedback"
-                                      fullWidth
-                                      multiline
-                                      rows={4}
-                                      variant="outlined"
-                                      value={feedback}
-                                      onChange={(e) =>
-                                        setFeedback(e.target.value)
-                                      }
-                                    />
-                                  </DialogContent>
-                                  <DialogActions>
-                                    <Button
-                                      onClick={handleClose}
-                                      color="secondary"
-                                    >
-                                      Cancel
-                                    </Button>
-                                    <Button
-                                      onClick={() => handleReject(lastDoc._id)}
-                                      variant="contained"
-                                      color="error"
-                                      disabled={!feedback.trim()} // Disable if feedback is empty
-                                    >
-                                      Submit
-                                    </Button>
-                                  </DialogActions>
-                                </Dialog>
-                              </>
-                            );
-                          case "Approved":
-                            return (
-                              <Button
-                                variant="contained"
-                                color="primary"
-                                size="small"
-                                onClick={() =>
-                                  handleSendNotification(
-                                    app.user.email,
-                                    nextStep[lastDoc.type]
-                                  )
-                                }
+                  <TableCell sx={{ width: "30%" }}>
+                    {app.documents && app.documents.length > 0
+                      ? app.documents
+                          .filter((doc) => doc.status === "Approved")
+                          .map((doc, index) => (
+                            <div key={index}>
+                              <Link
+                                href={baseUrl + "/uploads/" + doc.file}
+                                target="_blank"
+                                rel="noopener noreferrer"
                               >
-                                Send Notification
-                              </Button>
-                            );
-                          default:
-                            return null;
-                        }
-                      })()
-                    ) : (
-                      <Button
-                        variant="contained"
-                        color="primary"
-                        size="small"
-                        onClick={() =>
-                          handleSendNotification(app.user.email, "OPT Receipt")
-                        }
-                      >
-                        Send Notification
-                      </Button>
-                    )}
+                                {doc.type}
+                              </Link>
+                            </div>
+                          ))
+                      : null}
                   </TableCell>
                 </TableRow>
               ))
